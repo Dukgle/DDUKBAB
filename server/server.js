b278d0a906f6707e75c84a6f6aadf7c504c0ee62
pookey1104 <pookey1104@naver.com>
2023-10-10 07:41:27 +0900

    [MOD] BE CORS 설정 #pookey1104

--- server/server.js
+++ server/server.js
@@ -26,12 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS 설정
-const corsOptions = {
-  origin: "https://ddukbab.netlify.app",
-  optionsSuccessStatus: 200, // 일부 오래된 브라우저들을 위한 옵션
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // API 라우트 추가
 app.use("/api", users);
