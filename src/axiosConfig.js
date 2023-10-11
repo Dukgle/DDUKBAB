be47ea8cf0d555038fe8c1d91966672aad56a155
pookey1104 <pookey1104@naver.com>
2023-10-11 12:57:31 +0900

    [MOD] axios 설정 수정2 #pookey1104

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "process.env.REACT_APP_API_URL", // 백엔드 주소 설정
+  //   baseURL: "process.env.REACT_APP_API_URL", // 백엔드 주소 설정
   withCredentials: true, // CORS 요청에 필요한 옵션
 });
 
