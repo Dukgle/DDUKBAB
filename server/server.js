e2dab3c1d051dde3421330c17f4b786a8b3fd6cd
pookey1104 <pookey1104@naver.com>
2023-10-11 12:32:12 +0900

    [MOD] CORS 수정 #pookey1104

--- server/server.js
+++ server/server.js
@@ -1,31 +1,46 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 
-const bodyParser = require('body-parser');
-const users = require('./routes/users');
-const posts = require('./routes/posts');
-const my_page = require('./routes/my_page')
-const seat_reserve = require('./routes/seat_reserve')
-const user_change = require('./routes/user_change')
-const bookmark = require('./routes/bookmark');
-const shopping = require('./routes/shopping')
-const order = require('./routes/order')
-const point_charge = require('./routes/point_charge');
+const bodyParser = require("body-parser");
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+const my_page = require("./routes/my_page");
+const seat_reserve = require("./routes/seat_reserve");
+const user_change = require("./routes/user_change");
+const bookmark = require("./routes/bookmark");
+const shopping = require("./routes/shopping");
+const order = require("./routes/order");
+const point_charge = require("./routes/point_charge");
 
-const saler_page = require('./routes/saler/saler_page')
-const saler_change = require('./routes/saler/saler_change')
-const saler_menu = require('./routes/saler/menu')
+const saler_page = require("./routes/saler/saler_page");
+const saler_change = require("./routes/saler/saler_change");
+const saler_menu = require("./routes/saler/menu");
 
 const app = express();
+
+// CORS 설정
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://ddukbab.netlify.app", "https://ddukbab-160bd6fc13f3.herokuapp.com"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Body 파서 설정
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+/*
 // 허용 가능한 호스트를 설정
 const allowedHosts = ["https://ddukbab.netlify.app", "https://ddukbab-160bd6fc13f3.herokuapp.com"];
 
@@ -42,6 +57,7 @@ app.use(
     credentials: true,
   })
 );
+*/
 
 /*
 // CORS 설정
@@ -70,16 +86,15 @@ app.options("*", (req, res) => {
 */
 
 // API 라우트 추가
-app.use('/api', users);
-app.use('/api/users', user_change);
-app.use('/api/users', my_page);
-app.use('/api/users/reservation', seat_reserve);
-app.use('/api/users/posts', posts);
-app.use('/api/users/bookmarks', bookmark);
-app.use('/api/users/shopping', shopping);
-app.use('/api/users/order', order); // 많은 수량으로 인해 자동적으로 품절 처리 ? 가능하면 구현
-app.use('/api/user', point_charge);
-
+app.use("/api", users);
+app.use("/api/users", user_change);
+app.use("/api/users", my_page);
+app.use("/api/users/reservation", seat_reserve);
+app.use("/api/users/posts", posts);
+app.use("/api/users/bookmarks", bookmark);
+app.use("/api/users/shopping", shopping);
+app.use("/api/users/order", order); // 많은 수량으로 인해 자동적으로 품절 처리 ? 가능하면 구현
+app.use("/api/user", point_charge);
 
 app.use("/api/salers", saler_menu);
 app.use("/api/salers", saler_page);
